cc26093531f9b27c7e1e7abe32226894f4f79d53
Charles Javerliat <charles.javerliat@insa-lyon.fr>
2019-12-26 15:25:35 +0100

    feat/Add FiltreTrajets

---
+++ src/FiltreTrajets.cpp
@@ -0,0 +1,174 @@
+/*************************************************************************
+FiltreTrajets  -  description
+-------------------
+début                : 18/12/2019
+copyright            : (C) 2019 par Charles Javerliat, Fabien Narboux
+e-mail               : charles.javerliat@insa-lyon.fr, fabien.narboux@insa-lyon.fr
+*************************************************************************/
+
+//---------- Réalisation de la classe <FiltreTrajets> (fichier FiltreTrajets.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+#include <string.h>
+//------------------------------------------------------ Include personnel
+#include "FiltreTrajets.h"
+#include "Trajet.h"
+#include "TrajetSimple.h"
+#include "TrajetCompose.h"
+#include "MaillonListeChaineeTrajets.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+
+ListeChaineeTrajets* FiltreTrajets::filtrerParType(ListeChaineeTrajets& trajets, TypeTrajet typeTrajet) const
+{
+  ListeChaineeTrajets* listeFiltree = new ListeChaineeTrajets;
+
+  MaillonListeChaineeTrajets* maillonAct = trajets.getPremierMaillon();
+
+  while(maillonAct != nullptr)
+  {
+    //Trajet actuel
+    Trajet* t = maillonAct->getTrajet();
+
+    //Si le type de filtrage est SIMPLE et que le trajet est une instance de TrajetSimple, on l'ajoute à la nouvelle liste
+    //Si le type de filtrage est COMPOSE et que le trajet est une instance de TrajetCompose, on l'ajoute à la nouvelle liste
+    if((typeTrajet == TypeTrajet::SIMPLE && dynamic_cast<TrajetSimple*>(t) != nullptr)
+    || (typeTrajet == TypeTrajet::COMPOSE && dynamic_cast<TrajetCompose*>(t) != nullptr))
+    {
+      listeFiltree->ajouter(t);
+    }
+
+    maillonAct = maillonAct->getMaillonSuivant();
+  }
+
+  return listeFiltree;
+} //----- Fin de filtrerParType
+
+
+ListeChaineeTrajets* FiltreTrajets::filtrerParVilleDepart(ListeChaineeTrajets& trajets, const std::string& villeDepart) const
+{
+  ListeChaineeTrajets* listeFiltree = new ListeChaineeTrajets;
+
+  MaillonListeChaineeTrajets* maillonAct = trajets.getPremierMaillon();
+
+  while(maillonAct != nullptr)
+  {
+    //Trajet actuel
+    Trajet* t = maillonAct->getTrajet();
+
+    //Si la ville de départ est strictement égale (comparaison caractère par caractère en interne)
+    //au paramètre villeDepart du filtre, on l'ajoute à la nouvelle liste
+    if(t->getVilleDepart() == villeDepart)
+    {
+      listeFiltree->ajouter(t);
+    }
+    maillonAct = maillonAct->getMaillonSuivant();
+  }
+
+  return listeFiltree;
+} //----- Fin de filtrerParVilleDepart
+
+
+ListeChaineeTrajets* FiltreTrajets::filtrerParVilleArrivee(ListeChaineeTrajets& trajets, const std::string& villeArrivee) const
+{
+  ListeChaineeTrajets* listeFiltree = new ListeChaineeTrajets;
+
+  MaillonListeChaineeTrajets* maillonAct = trajets.getPremierMaillon();
+
+  while(maillonAct != nullptr)
+  {
+    //Trajet actuel
+    Trajet* t = maillonAct->getTrajet();
+
+    //Si la ville d'arrivée est strictement égale (comparaison caractère par caractère en interne)
+    //au paramètre villeArrivee du filtre, on l'ajoute à la nouvelle liste
+    if(t->getVilleArrivee() == villeArrivee)
+    {
+      listeFiltree->ajouter(t);
+    }
+
+    maillonAct = maillonAct->getMaillonSuivant();
+  }
+  return listeFiltree;
+} //----- Fin de filtrerParVilleArrivee
+
+
+ListeChaineeTrajets* FiltreTrajets::filtrerParVilleDepartEtArrivee(ListeChaineeTrajets& trajets, const std::string& villeDepart, const std::string& villeArrivee) const
+{
+  ListeChaineeTrajets* listeFiltree = new ListeChaineeTrajets;
+
+  MaillonListeChaineeTrajets* maillonAct = trajets.getPremierMaillon();
+
+  while(maillonAct != nullptr)
+  {
+    //Trajet actuel
+    Trajet* t = maillonAct->getTrajet();
+
+    //Si la ville d'arrivée et de départ sont strictements égales (comparaison caractère par caractère en interne)
+    //aux paramètres villeDepart et villeArrivee du filtre, on l'ajoute à la nouvelle liste
+    if(t->getVilleArrivee() == villeArrivee && t->getVilleDepart() == villeDepart)
+    {
+      listeFiltree->ajouter(t);
+    }
+    maillonAct = maillonAct->getMaillonSuivant();
+  }
+
+  return listeFiltree;
+} //----- Fin de filtrerParVilleDepartEtArrivee
+
+
+ListeChaineeTrajets* FiltreTrajets::filtrerParIntervalle(ListeChaineeTrajets& trajets, const unsigned int n, const unsigned int m) const
+{
+  ListeChaineeTrajets* listeFiltree = new ListeChaineeTrajets;
+
+  MaillonListeChaineeTrajets* maillonAct = trajets.getPremierMaillon();
+
+  unsigned int cpt(0);
+
+  while(maillonAct != nullptr)
+  {
+    //Trajet actuel
+    Trajet* t = maillonAct->getTrajet();
+
+    //Si le l'indice du trajet se trouve dans l'intervalle, on l'ajoute à la nouvelle liste
+    if(cpt >= n || cpt <= m) {
+      listeFiltree->ajouter(t);
+    }
+
+    maillonAct = maillonAct->getMaillonSuivant();
+    ++cpt;
+  }
+
+  return listeFiltree;
+} //----- Fin de filtrerParIntervalle
+
+//-------------------------------------------- Constructeurs - destructeur
+
+
+FiltreTrajets::FiltreTrajets ( )
+{
+  #ifdef MAP
+  cout << "Appel au constructeur de <FiltreTrajets>" << endl;
+  #endif
+} //----- Fin de FiltreTrajets
+
+
+FiltreTrajets::~FiltreTrajets ( )
+{
+  #ifdef MAP
+  cout << "Appel au destructeur de <FiltreTrajets>" << endl;
+  #endif
+} //----- Fin de ~FiltreTrajets
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
---
+++ src/FiltreTrajets.h
@@ -0,0 +1,102 @@
+/*************************************************************************
+FiltreTrajets  -  Filtre de trajets
+-------------------
+début                : 18/12/2019
+copyright            : (C) 2019 par Charles Javerliat, Fabien Narboux
+e-mail               : charles.javerliat@insa-lyon.fr, fabien.narboux@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface de la classe <FiltreTrajets> (fichier FiltreTrajets.h) ----------------
+#if ! defined ( FILTRE_TRAJETS_H )
+#define FILTRE_TRAJETS_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+#include <string>
+
+#include "ListeChaineeTrajets.h"
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+typedef enum TypeTrajet_ {
+  SIMPLE,
+  COMPOSE
+} TypeTrajet;
+
+//------------------------------------------------------------------------
+// Rôle de la classe <FiltreTrajets>
+//
+// La classe FiltreTrajets permet de filtrer une ListeChaineeTrajets selon
+// des critères particuliers (ville de départ, d'arrivée, type du trajet, ...)
+// et de renvoyer les extraits sous forme de nouvelles instances de ListeChaineeTrajets.
+//------------------------------------------------------------------------
+
+class FiltreTrajets
+{
+  //----------------------------------------------------------------- PUBLIC
+
+public:
+  //----------------------------------------------------- Méthodes publiques
+
+  ListeChaineeTrajets* filtrerParType(ListeChaineeTrajets& trajets, TypeTrajet typeTrajet) const;
+  // Mode d'emploi : Filtre la liste de trajets donnée en paramètre. Renvoie une nouvelle instance
+  // de ListeChaineeTrajets contenant uniquement les trajets de type: typeTrajet.
+  //
+  // Contrat :
+  //  - Renvoie une ListeChaineeTrajets contenant uniquement des trajets de type: typeTrajet.
+  //  - Renvoie une liste vide si aucun trajet n'a passé le filtre.
+  //
+
+  ListeChaineeTrajets* filtrerParVilleDepart(ListeChaineeTrajets& trajets, const std::string& villeDepart) const;
+  // Mode d'emploi : Filtre la liste de trajets donnée en paramètre. Renvoie une nouvelle instance
+  // de ListeChaineeTrajets contenant uniquement les trajets ayant pour ville de départ: villeDepart.
+  //
+  // Contrat :
+  //  - Renvoie une ListeChaineeTrajets contenant uniquement des trajets ayant pour ville de départ: villeDepart.
+  //  - Renvoie une liste vide si aucun trajet n'a passé le filtre.
+  //
+
+  ListeChaineeTrajets* filtrerParVilleArrivee(ListeChaineeTrajets& trajets, const std::string& villeArrivee) const;
+  // Mode d'emploi : Filtre la liste de trajets donnée en paramètre. Renvoie une nouvelle instance
+  // de ListeChaineeTrajets contenant uniquement les trajets ayant pour ville d'arrivée: villeArrivee.
+  //
+  // Contrat :
+  //  - Renvoie une ListeChaineeTrajets contenant uniquement des trajets ayant pour ville d'arrivée: villeArrivee.
+  //  - Renvoie une liste vide si aucun trajet n'a passé le filtre.
+  //
+
+  ListeChaineeTrajets* filtrerParVilleDepartEtArrivee(ListeChaineeTrajets& trajets, const std::string& villeDepart, const std::string& villeArrivee) const;
+  // Mode d'emploi : Filtre la liste de trajets donnée en paramètre. Renvoie une nouvelle instance
+  // de ListeChaineeTrajets contenant uniquement les trajets ayant pour ville de départ: villeDepart; et pour ville d'arrivée: villeArrivee.
+  //
+  // Contrat :
+  //  - Renvoie une ListeChaineeTrajets contenant uniquement des trajets ayant pour ville de départ: villeDepart; et pour ville d'arrivée: villeArrivee.
+  //  - Renvoie une liste vide si aucun trajet n'a passé le filtre.
+  //
+
+  ListeChaineeTrajets* filtrerParIntervalle(ListeChaineeTrajets& trajets, const unsigned int n, const unsigned int m) const;
+  // Mode d'emploi : Filtre la liste de trajets donnée en paramètre. Renvoie une nouvelle instance
+  // de ListeChaineeTrajets contenant uniquement les trajets d'indice allant de n à m inclus.
+  //
+  // Contrat :
+  //  - Renvoie une ListeChaineeTrajets contenant au maximum (m-n+1) trajets.
+  //  - Renvoie une liste vide si aucun trajet n'a passé le filtre.
+  //
+
+  FiltreTrajets ( );
+
+  virtual ~FiltreTrajets ( );
+
+  //------------------------------------------------------------------ PRIVE
+
+protected:
+  //----------------------------------------------------- Méthodes protégées
+
+  //----------------------------------------------------- Attributs protégés
+};
+
+//-------------------------------- Autres définitions dépendantes de <FiltreTrajets>
+
+#endif // FILTRE_TRAJETS_H
